ffb30db1af15f33a5d8cae1b4014c8683c8011c6
stivex001 <stephenadeyemo@gmail.com>
2023-11-13 03:05:37 +0100

    feat: added new contents

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Navlinks from "./Navlinks";
 import logo from "@/public/assets/2.png";
+import Image from "next/image";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div className="bg-[#1D974A]/70  w-full py-9 fixed z-10 top-0">
-      <div className="w-[80%] mx-auto flex items-center justify-between text-white">
-        {/* logo */}
-        logo
+    <div className="bg-[#1D974A]/60  w-full py-9 fixed z-10 top-0">
+      <div className="w-[80%] mx-auto flex items-center justify-between text-white gap-[100px]">
+        <div className="w-20 h-10 flex justify-center items-center">
+          <Image src={logo} alt="logo" className=" object-cover w-full " />
+        </div>
         <Navlinks />
       </div>
     </div>
--- components/Navlinks.tsx
+++ components/Navlinks.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-type Props = {}
+const navLinks = [
+  { id: 1, title: "Home", url: "/" },
+  { id: 2, title: "About Us", url: "/about" },
+  { id: 3, title: "Our Products", url: "/products" },
+  { id: 4, title: "Projects", url: "/projects" },
+  { id: 5, title: "Services", url: "/services" },
+  { id: 6, title: "News", url: "/news" },
+  { id: 7, title: "Contact Us", url: "/contact" },
+];
+
+type Props = {};
 
 const Navlinks = (props: Props) => {
   return (
-    <div>Navlinks</div>
-  )
-}
+    <nav className="">
+      <ul className="flex">
+        {navLinks.map((link) => (
+          <Link href={link.url} key={link.id}>
+            <span className="text-white hover:text-gray-300">{link.title}</span>
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Navlinks
+export default Navlinks;
