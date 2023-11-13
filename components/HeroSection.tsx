2c9a3e689f75fe9424e850f8756527aad76b16f1
stivex001 <stephenadeyemo@gmail.com>
2023-11-13 02:34:51 +0100

    feat:implemented hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const HeroSection = (props: Props) => {
   return (
-    <section className="flex h-screen bg-hero-section bg-no-repeat bg-cover brightness-100 justify-center items-center">
+    <section className="flex h-screen bg-hero-section bg-no-repeat bg-cover bg-center brightness-100 justify-center items-center w-full ">
       <div className="text-white font-bold text-7xl">Welcome to Mike Rik</div>
     </section>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import Navlinks from './Navlinks'
+import React from "react";
+import Navlinks from "./Navlinks";
+import logo from "@/public/assets/2.png";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div className='bg-[#00683D] w-full py-9 fixed z-10 top-0'>
-        <div className='w-5/6 mx-auto flex items-center justify-between text-white'>
-            {/* logo */}
-            logo
-            <Navlinks />
-        </div>
+    <div className="bg-[#1D974A]/70  w-full py-9 fixed z-10 top-0">
+      <div className="w-[80%] mx-auto flex items-center justify-between text-white">
+        {/* logo */}
+        logo
+        <Navlinks />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
