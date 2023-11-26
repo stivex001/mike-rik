f0cccb491f0b822618ecee9786e80e9839b35606
stivex001 <stephenadeyemo@gmail.com>
2023-11-26 12:40:31 +0100

    contents adjusted

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,15 +5,15 @@ type Props = {};
 const HeroSection = (props: Props) => {
   return (
     <section className="flex h-screen bg-hero-section bg-no-repeat bg-cover bg-center brightness-100  items-center w-full ">
-      <div className="flex flex-col w-5/6 mx-auto mt-16">
+      <div className="flex flex-col w-5/6 mx-auto mt-28 md:mt-16">
         <h2 className="text-white text-2xl font-bold mb-5">
           Original & Natural
         </h2>
-        <h1 className="text-orange-300 text-7xl font-bold mb-3">
+        <h1 className="text-orange-300 text-4xl md:text-7xl font-extrabold md:font-bold mb-3">
           Agriculture Matter
         </h1>
-        <h1 className="text-white text-7xl font-bold ">Good production</h1>
-        <span className="max-w-3xl text-white text-xl font-normal my-5">
+        <h1 className="text-white text-4xl font-extrabold md:text-7xl md:font-bold ">Good production</h1>
+        <span className="w-full md:max-w-3xl text-white md:text-xl font-normal my-5">
           Our success is built upon the knowledge of the market and wonderful
           relationship with our suppliers and customers.
         </span>
