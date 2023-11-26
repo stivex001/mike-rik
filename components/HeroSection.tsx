7c92c88193ad825800f99d0c34811ed6f23d2eb7
stivex001 <stephenadeyemo@gmail.com>
2023-11-26 12:33:32 +0100

    feat:implemented hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,8 +4,20 @@ type Props = {};
 
 const HeroSection = (props: Props) => {
   return (
-    <section className="flex h-screen bg-hero-section bg-no-repeat bg-cover bg-center brightness-100 justify-center items-center w-full ">
-      <div className="text-white font-bold text-7xl w-[80%] mx-auto">Welcome to Mike Rik</div>
+    <section className="flex h-screen bg-hero-section bg-no-repeat bg-cover bg-center brightness-100  items-center w-full ">
+      <div className="flex flex-col w-5/6 mx-auto mt-16">
+        <h2 className="text-white text-2xl font-bold mb-5">
+          Original & Natural
+        </h2>
+        <h1 className="text-orange-300 text-7xl font-bold mb-3">
+          Agriculture Matter
+        </h1>
+        <h1 className="text-white text-7xl font-bold ">Good production</h1>
+        <span className="max-w-3xl text-white text-xl font-normal my-5">
+          Our success is built upon the knowledge of the market and wonderful
+          relationship with our suppliers and customers.
+        </span>
+      </div>
     </section>
   );
 };
